9ffc354dbe64b3cc9a0735acb76031d2d946e0ff
ppPacific <90137395+ppPacific@users.noreply.github.com>
2023-02-15 17:40:56 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <nav className="home">
+            <h2>Home</h2>
+            {/*<div className="links">*/}
+            {/*    <a href="/">Home</a>*/}
+            {/*    <a href="/create">New</a>*/}
+            {/*</div>*/}
+        </nav>
+    );
+};
+
+export default Home;
---
+++ Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h2>Here to stay</h2>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
